910ec14e575935f3196eae405766fc056c216476
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-10 09:35:33 -0600

    Create a method to add exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,17 +1,33 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
   def test_it_exists
     dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_instance_of Museum, patron_1
+    assert_instance_of Museum, dmns
   end
 
   def test_it_has_attributes
     dmns = Museum.new("Denver Museum of Nature and Science")
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+
+  def test_it_can_add_interests
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    assert_equal [], dmns.exhibits
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
 end
