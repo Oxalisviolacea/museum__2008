51eda0ce5848f7c898811c8a9be9667593b9f136
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-10 10:04:47 -0600

    Test it has attributes

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
 require './lib/exhibit'
+require './lib/patron'
 
 class MuseumTest < Minitest::Test
   def test_it_exists
@@ -14,20 +15,5 @@ class MuseumTest < Minitest::Test
     dmns = Museum.new("Denver Museum of Nature and Science")
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
-  end
-
-  def test_it_can_add_interests
-    dmns = Museum.new("Denver Museum of Nature and Science")
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-
-    assert_equal [], dmns.exhibits
-
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
 end
