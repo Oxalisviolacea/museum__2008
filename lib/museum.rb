93e1f3565183d0f6f40f7c6e7f8ada6f6ddadae1
Hanna Davis <hanna.e.davis@gmail.com>
2020-09-10 10:07:29 -0600

    Create method to reccomend exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,14 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    reccomended_exhibits = []
+    @exhibits.map do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        reccomended_exhibits << exhibit
+      end
+    end
+    reccomended_exhibits
+  end
 end
